7c3c23eda9d2ebe2d77d7e43b2644533ffd5f6da
RobsonMT <rbsndev3@gmail.com>
2022-06-01 13:06:40 -0300

    :rocket: deploy on heroku
    deploy on heroku

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,13 +7,9 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PWD,
-  database: process.env.POSTGRES_DB,
-  synchronize: true,
+  url: process.env.DATABASE_URL,
   logging: false,
+  ssl: { rejectUnauthorized: false },
   entities: [path.join(__dirname, "entities/**/*.{ts,js}")],
   migrations: [path.join(__dirname, "migrations/**/*.{ts,js}")],
 });
